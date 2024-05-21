1934963796f0bcbf6c45a443143246d5384383bf
fernacho <romero11780@gmail.com>
2024-05-21 15:29:56 -0600

    cambios

--- ProyectoCudec1/frmCompra.aspx.cs
+++ ProyectoCudec1/frmCompra.aspx.cs
@@ -73,27 +73,21 @@ namespace ProyectoCudec1
 
         protected void btnEliminar_Click(object sender, EventArgs e)
         {
-            int idCompra = Convert.ToInt32((sender as Button).CommandArgument);
+            int idProducto = Convert.ToInt32((sender as Button).CommandArgument);
 
-            var compra = _context.Compra.FirstOrDefault(c => c.idCompra == idCompra && c.idusuario == WebForm3.UserId);
+            // Buscar la compra asociada al idProducto
+            var compra = _context.Compra.FirstOrDefault(c => c.idproducto == idProducto && c.idusuario == WebForm3.UserId);
+
             if (compra != null)
             {
-                // Verificar si hay envíos relacionados
-                var enviosRelacionados = _context.Envio.Where(envio => envio.idCompra == idCompra).ToList();
-
-                // Manejar los envíos relacionados
-                foreach (var envio in enviosRelacionados)
-                {
-                    _context.Envio.Remove(envio);
-                }
-
                 // Eliminar la compra
                 _context.Compra.Remove(compra);
                 _context.SaveChanges();
-            }
 
-            CargarCarrito();
+                CargarCarrito();
+            }
         }
+
 
         protected void btnComprar_Click(object sender, EventArgs e)
         {
